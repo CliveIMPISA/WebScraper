3f5147835c064191f46f35c9ae6cc008b8f4561d
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2014-10-11 21:03:57 +0800

    accepting command line arguments

--- contracts.rb
+++ contracts.rb
@@ -1,12 +1,12 @@
 
-# fail ArgumentError, "Usage: badges [username]\n" if ARGV.count == 0
-#user_name = ARGV[0reuire
+fail ArgumentError, "Usage: team [teamname]\n" if ARGV.count == 0
+team_name = ARGV[0]
 require_relative 'salaryscrape'
 
 code=BasketballReference.new()
 
-contracts_data = code.get_team("MIA")
+contracts_data = code.get_team(team_name)
 
 contracts_text = code.just_text(contracts_data)
-      
+
 code.display(contracts_text)

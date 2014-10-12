5fcace9a8ec7d26e16d2ce7d725d89cd799e96b2
CliveIMPISA <augustinclive@hotmail.com>
2014-10-13 04:25:13 +0800

    Re-adjusted and inclusion of module

--- contracts.rb
+++ contracts.rb
@@ -4,9 +4,10 @@ Please check the readme file for team names abbreviation\n" if ARGV.count == 0
 
 team_name = ARGV[0]
 require_relative 'salaryscrape'
+include SalaryScraper
 
 code = BasketballReference.new
 
 # code.get_team(team_name)
 
-code.to_yaml(code.to_array_of_hashes(team_name))
+code.to_yaml(team_name)
--- salaryscrape.rb
+++ salaryscrape.rb
@@ -2,66 +2,69 @@ require 'nokogiri'
 require 'open-uri'
 require 'yaml'
 
-# Class Pulls and Manipulates data from basketball-reference.com
-class BasketballReference
-  WEBSITE = 'http://www.basketball-reference.com/contracts'
-  PAYROLL = "//div[@id='div_payroll']//tbody//tr[@class='']//td"
-  HEAD = "//div[@id='div_payroll']//thead//tr[@class='']//th"
+module SalaryScraper
+  # Class Pulls and Manipulates data from basketball-reference.com
+  class BasketballReference
+    WEBSITE = 'http://www.basketball-reference.com/contracts'
+    PAYROLL = "//div[@id='div_payroll']//tbody//tr[@class='']//td"
+    HEAD = "//div[@id='div_payroll']//thead//tr[@class='']//th"
 
-  def get_team(team)
-    doc = get_page(team)
-    doc.xpath(PAYROLL)
-  end
-
-  def get_page(team)
-    url = "#{WEBSITE}/#{team}.html"
-    Nokogiri::HTML(open(url))
-  end
-
-  def just_text(data)
-    textes_array = []
-    data.each { |t| textes_array << t.text }
-    textes_array
-  end
-
-  def get_column_heads(team)
-    doc = get_page(team)
-    doc.xpath(HEAD)
-  end
-
-  def to_array_of_hashes(team)
-    players_data_array = []
-    head_array = []
-
-    players_data = get_team(team)
-    head = get_column_heads(team)
-
-    players_data.each { |x| players_data_array << x.text }
-    head.each { |x| head_array << x.text }
-    number_arrays = get_number_arrays(players_data_array, head_array)
-    assign_key_to_value(number_arrays, players_data_array, head_array)
-  end
-
-  def assign_key_to_value(number_arrays, players_data_array, head_array)
-    data = []
-    number_arrays.times do
-      piece_of_data = Hash.new
-      head_array.each do |x|
-        piece_of_data[x] = players_data_array.first
-        players_data_array.shift
-      end
-      data << piece_of_data
+    def get_team(team)
+      doc = get_page(team)
+      doc.xpath(PAYROLL)
     end
-    data
-  end
 
-  def get_number_arrays(val1, val2)
-    val1.size / val2.size
-  end
+    def get_page(team)
+      url = "#{WEBSITE}/#{team}.html"
+      Nokogiri::HTML(open(url))
+    end
 
-  def to_yaml(data)
-    File.open('test.yml', 'w') do  |file|
-      file.puts data.to_yaml
+    def just_text(data)
+      textes_array = []
+      data.each { |t| textes_array << t.text }
+      textes_array
+    end
+
+    def get_column_heads(team)
+      doc = get_page(team)
+      doc.xpath(HEAD)
+    end
+
+    def to_array_of_hashes(team)
+      players_data_array = []
+      head_array = []
+
+      players_data = get_team(team)
+      head = get_column_heads(team)
+
+      players_data.each { |x| players_data_array << x.text }
+      head.each { |x| head_array << x.text }
+      number_arrays = get_number_arrays(players_data_array, head_array)
+      assign_key_to_value(number_arrays, players_data_array, head_array)
+    end
+
+    def assign_key_to_value(number_arrays, players_data_array, head_array)
+      data = []
+      number_arrays.times do
+        piece_of_data = Hash.new
+        head_array.each do |x|
+          piece_of_data[x] = players_data_array.first
+          players_data_array.shift
+        end
+        data << piece_of_data
+      end
+      data
+    end
+
+    def get_number_arrays(val1, val2)
+      val1.size / val2.size
+    end
+
+    def to_yaml(team_name)
+      data = to_array_of_hashes(team_name)
+      File.open('test.yml', 'w') do  |file|
+        file.puts data.to_yaml
+      end
     end
   end
 end
